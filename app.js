25d06292b5f5663164852117730a1caf0da3ca38
akunkosong <info.akunkosong@gmail.com>
2025-05-08 01:11:03 +0700

    4th commit

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
-require('./config/firebase'); // init Firebase Admin
+require('./config/firebase'); 
 
 app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('DIGISCHOOL API is running 🚀');
-  });
-  
+  res.send('DIGISCHOOL API is running 🚀');
+});
+
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/data', require('./routes/dataRoutes'));
 
-app.listen(3000, () => console.log('API running on http://localhost:3000'));
+
+module.exports = app;
